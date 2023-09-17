e11b3f8fdbeac0301c6f5fbceada078998d5a92a
ccz <15939054361@163.com>
2023-09-17 09:48:44 +0800

    feat: 套壳

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import Versions from './components/Versions.vue'
+</script>
+
+<template>
+  <iframe src="https://douyin-31xm.onrender.com" frameborder="0"></iframe>
+</template>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
